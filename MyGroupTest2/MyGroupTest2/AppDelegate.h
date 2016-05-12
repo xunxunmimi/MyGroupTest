e794dd1ba445e871a9dddad77de15fb050f0dd70
lanou3g <1135447344@qq.com>
2016-05-12 13:20:55 +0800

    第一次

---
+++ MyGroupTest2/MyGroupTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGroupTest2
+//
+//  Created by lanou3g on 16/5/12.
+//  Copyright © 2016年 WZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGroupTest2/MyGroupTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGroupTest2
+//
+//  Created by lanou3g on 16/5/12.
+//  Copyright © 2016年 WZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
